93dda2d7c8f014261dc1f50f8de69267f830877c
kaushik <kaushik.movaliya48@gmail.com>
2017-09-23 19:05:14 +0530

    working on video gallery

--- MirchMasala/VideoGallaryView.m
+++ MirchMasala/VideoGallaryView.m
@@ -11,15 +11,19 @@
 #import "AVKit/AVKit.h"
 #import "AVFoundation/AVFoundation.h"
 #import "VideoCell.h"
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface VideoGallaryView ()
 {
     NSMutableArray *ImageNameSection;
+    NSMutableArray *VideoArr;
 }
 @end
 
 @implementation VideoGallaryView
+
 @synthesize VideoTBL;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -32,6 +36,7 @@
     
     [VideoTBL registerNib:nib forCellReuseIdentifier:@"VideoCell"];
     
+    [self VideoGalleryService];
     /*
     NSURL *url = [[NSURL alloc] initWithString:[NSString stringWithFormat:@"https://youtu.be/wWUzXFG_F4g"]];
     
@@ -41,10 +46,109 @@
     [self presentViewController:_moviePlayer1 animated:YES completion:^{
         [_moviePlayer1.player play];
     }];*/
-    
-    
+}
 
+-(void)setImage:(NSString*)urlResponse
+{
+    NSString *youtubeUrl = [NSString stringWithFormat:@"%@",[self parseVideoHTMLUrl: urlResponse]];
     
+    NSString *videoThumbUrl = [self getYoutubeVideoThumbnail: youtubeUrl];
+    
+    NSURL* videoURL =[NSURL URLWithString:videoThumbUrl];
+    NSLog(@"");
+    
+   // [btn_photo sd_setImageWithURL:videoURL forState:UIControlStateNormal];
+}
+
+-(NSString*)getYoutubeVideoThumbnail:(NSString*)youTubeUrl
+{
+    NSString* video_id = @"";
+    
+    if (youTubeUrl.length > 0)
+    {
+        NSError *error = NULL;
+        NSRegularExpression *regex =
+        [NSRegularExpression regularExpressionWithPattern:@"(?<=watch\\?v=|/videos/|embed\\/)[^#\\&\\?]*"
+                                                  options:NSRegularExpressionCaseInsensitive
+                                                    error:&error];
+        NSTextCheckingResult *match = [regex firstMatchInString:youTubeUrl
+                                                        options:0
+                                                          range:NSMakeRange(0, [youTubeUrl length])];
+        if (match)
+        {
+            NSRange videoIDRange = [match rangeAtIndex:0];
+            video_id = [youTubeUrl substringWithRange:videoIDRange];
+            
+            NSLog(@"%@",video_id);
+        }
+    }
+    
+    NSString* thumbImageUrl = [NSString stringWithFormat:@"http://img.youtube.com/vi/%@/default.jpg",video_id];
+    
+    return thumbImageUrl;
+}
+
+-(NSString *)parseVideoHTMLUrl:(NSString *)videoUrl
+{
+    NSRegularExpression *regex = [NSRegularExpression
+                                  regularExpressionWithPattern:@";//(.+?)\\&quot;"
+                                  options:NSRegularExpressionCaseInsensitive
+                                  error:nil];
+    NSTextCheckingResult *textCheckingResult = [regex firstMatchInString:videoUrl options:0 range:NSMakeRange(0, videoUrl.length)];
+    
+    NSString *url = [videoUrl substringWithRange:[textCheckingResult rangeAtIndex:1]];
+    
+    return url;
+}
+
+-(void)VideoGalleryService
+{
+    [KVNProgress show] ;
+    NSMutableDictionary *dict1 = [[NSMutableDictionary alloc] init];
+    
+    [dict1 setValue:KAPIKEY forKey:@"APIKEY"];
+    
+    NSMutableDictionary *dictSub = [[NSMutableDictionary alloc] init];
+    [dictSub setObject:@"getitem" forKey:@"MODULE"];
+    [dictSub setObject:@"videoGallery" forKey:@"METHOD"];
+    
+    NSMutableArray *arr = [[NSMutableArray alloc] initWithObjects:dictSub, nil];
+    NSMutableDictionary *dictREQUESTPARAM = [[NSMutableDictionary alloc] init];
+    
+    [dictREQUESTPARAM setObject:arr forKey:@"REQUESTPARAM"];
+    [dictREQUESTPARAM setObject:dict1 forKey:@"RESTAURANT"];
+    
+    
+    NSError* error = nil;
+    
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dictREQUESTPARAM options:NSJSONWritingPrettyPrinted error:&error];
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:&error];
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.responseSerializer.acceptableContentTypes=[NSSet setWithObjects:@"text/html",@"application/json", nil];
+    AFJSONRequestSerializer *serializer = [AFJSONRequestSerializer serializer];
+    [serializer setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [serializer setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    manager.requestSerializer = serializer;
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    
+    [manager POST:kBaseURL parameters:json success:^(AFHTTPRequestOperation *operation, NSDictionary *responseObject)
+     {
+         [KVNProgress dismiss];
+         
+         NSString *SUCCESS=[[[[responseObject objectForKey:@"RESPONSE"] objectForKey:@"getitem"] objectForKey:@"videoGallery"] objectForKey:@"SUCCESS"];
+         if ([SUCCESS boolValue] ==YES)
+         {
+             VideoArr=[[[[[[responseObject objectForKey:@"RESPONSE"] objectForKey:@"getitem"] objectForKey:@"videoGallery"] objectForKey:@"result"] objectForKey:@"videoGallery"] mutableCopy];
+             [self setImage:@"https://www.youtube.com/watch?v=b_J7ByLJw5A"];
+             [VideoTBL reloadData];
+         }
+     }
+          failure:^(AFHTTPRequestOperation *operation, NSError *error)
+     {
+         NSLog(@"Fail");
+         [KVNProgress dismiss] ;
+     }];
 }
 
 - (void)didReceiveMemoryWarning
